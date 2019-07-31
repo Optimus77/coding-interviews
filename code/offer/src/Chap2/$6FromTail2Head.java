64315b11d65cd68e6e1cd662919bdda03f0cdde7
Optimus77 <Optimus_7@163.com>
2019-07-31 17:27:00 +0800

    #5 #6

--- code/offer/src/Chap2/$6FromTail2Head.java
+++ code/offer/src/Chap2/$6FromTail2Head.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
  * 输入一个链表的头节点，从尾到头打印链表每个节点的值。
  */
 
-public class FromTail2Head {
+public class $6FromTail2Head {
     // 节点类的定义
     private class ListNode {
         int val;
